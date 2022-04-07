0fdb7da86a5b15f7c30860c3652462368108505b
Colleen Murray <colleenkhm@gmail.com>
2022-04-07 06:15:51 -0500

    cleaned up code, created generate site file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Portfolio Generator
+
+## Description
+A Node.js application that takes user input and creates an online portfolio based on the information provided
+
+## Built With
+- HTML
+- CSS
+- JavaScript
+- Node.js
+
+## Contributors
+Made by Colleen Murray
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const generateSite = require('./utils/generate-site.js');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
@@ -133,12 +134,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        // stopped here at uncommenting out this, end of 9.4.4
-        const pageHTML = generatePage(portfolioData);
-        fs.writeFile('index.html', generatePage(name, github), err => {
-        if (err) throw new Error(err);
-        console.log('Portfolio complete! Check out index.html to see the output!')
-    });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
